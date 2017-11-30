255cc586e30ba16c29e427a1c8aecb9508736305
Maurice Roy <mautayro@gmail.com>
2017-11-30 02:01:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   smallest = nil
   name_hash.each do |key, val|
-    if smallest == nil || smallest > val.to_i
+    if smallest == nil || smallest > val
       smallest = key
     end
   end
